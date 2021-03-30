f84c5bc9c59a1d30a31a05b02ae88c010fabce7c
Serena-tz <71140697+Serena-tz@users.noreply.github.com>
2021-03-30 17:01:50 +0800

    first

---
+++ pages/index/index.js
@@ -0,0 +1,24 @@
+Page({
+  data:{
+    // 静态数据
+    zyx:{
+      job:"歌手、演员",
+      bd:"1991年10月7日",
+      info:"天秤座/A型",
+      work:["老九门","好先生","功夫瑜伽","求婚大作战"],
+      pro:"张艺兴（LAY），1991年10月7日出生于湖南省长沙市，内地流行男歌手、影视演员，男子演唱组合EXO/EXO-M中国籍成员..."
+    }
+    // zyx:[]
+  },
+  // onLoad(){
+  //   wx.request({
+  //     url: 'http://localhost/1121/zyx.json',
+  //     success:(res)=>{
+  //       console.log(res.data)
+  //       this.setData({
+  //         zyx:res.data
+  //       })
+  //     }
+  //   })
+  // }
+});
